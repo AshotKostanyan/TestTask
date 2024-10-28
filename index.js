76370d8f8713fb05c0177f95f53cf3cadb2ceaea
Ashot <kostanyanashot2007@gmail.com>
2024-10-28 16:38:53 +0400

    change combination generating route

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use('/api/routes', routes);
+app.use('/', routes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
